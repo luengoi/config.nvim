3bb8bf96f9bed2b44a6681c06c13a2db2f427f7f
Iván Luengo <28649633+luengoi@users.noreply.github.com>
2024-12-12 20:15:23 +0100

    add todo-comments plugin

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -304,4 +304,21 @@ return {
       },
     },
   },
+
+  -- finds and lists all TODO, HACK, BUG, etc comments
+  -- in your project and loads them into a browsable list
+  {
+    "folke/todo-comments.nvim",
+    cmd = { "TodoTrouble", "TodoTelescope" },
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      { "]t", function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
+      { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
+      { "<leader>xt", "<cmd>Trouble todo toggle<cr>", desc = "Todo (Trouble)" },
+      { "<leader>xT", "<cmd>Trouble todo toggle filter = {tag = {TODO,FIX,FIXME}}<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
+      { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
+      { "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
+    },
+  },
 }
